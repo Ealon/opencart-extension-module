996781d1beebbfc511cd15d3d4bde1fa3ed98ff6
Yilong <yhua4993@uni.sydney.edu.au>
2018-09-10 13:11:34 +1000

    - update

---
+++ admin/language/en-au/extension/module/my_module.php
@@ -0,0 +1,30 @@
+<?php
+// Heading
+$_['heading_title']     = 'MyModule';
+
+// Text
+$_['text_extension']    = 'Extensions';
+$_['text_success']      = 'Success: You have modified MyModule module!';
+$_['text_edit']         = 'Edit MyModule Module';
+
+// Entry
+$_['entry_name']        = 'Module Name';
+$_['entry_title']       = 'Section Title';
+$_['entry_description'] = 'Sub Title';
+$_['entry_background']  = 'Background';
+$_['entry_img1']        = 'Image 1';
+$_['button_text']       = 'Button Text';
+$_['link']              = 'Link';
+
+// Field 
+$_['field_title']       = 'Field Title';
+$_['field_content1']    = 'Content 1';
+$_['field_content2']    = 'Content 2';
+$_['field_content3']    = 'Content 3';
+
+$_['entry_status']      = 'Status';
+
+
+// Error
+$_['error_permission']  = 'Warning: You do not have permission to modify MyModule module!';
+$_['error_name']        = 'Module Name must be between 3 and 64 characters!';
--- catalog/controller/extension/module/my_module.php
+++ catalog/controller/extension/module/my_module.php
@@ -2,9 +2,13 @@
 class ControllerExtensionModuleMyModule extends Controller {
 	public function index($setting) {
 		if (isset($setting['module_description'][$this->config->get('config_language_id')])) {
-			$data['section_title'] = html_entity_decode($setting['module_description'][$this->config->get('config_language_id')]['title'], ENT_QUOTES, 'UTF-8');
+      
+      $data['section_title'] = html_entity_decode($setting['module_description'][$this->config->get('config_language_id')]['title'], ENT_QUOTES, 'UTF-8');
 			$data['description'] = html_entity_decode($setting['module_description'][$this->config->get('config_language_id')]['description'], ENT_QUOTES, 'UTF-8');
-			$data['column1_title'] = html_entity_decode($setting['module_description'][$this->config->get('config_language_id')]['column1_title'], ENT_QUOTES, 'UTF-8');
+			$data['background'] = html_entity_decode($setting['module_description'][$this->config->get('config_language_id')]['background'], ENT_QUOTES, 'UTF-8');
+			$data['img1'] = html_entity_decode($setting['module_description'][$this->config->get('config_language_id')]['img1'], ENT_QUOTES, 'UTF-8');
+			$data['fields'] = $setting['module_description'][$this->config->get('config_language_id')]['fields'];
+			$data['button_text'] = html_entity_decode($setting['module_description'][$this->config->get('config_language_id')]['button_text'], ENT_QUOTES, 'UTF-8');
 
 			return $this->load->view('extension/module/my_module', $data);
 		}
